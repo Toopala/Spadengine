4524b369f5d324984425a659e606044b233c764c
Toopala <miika.saastamoinen@gmail.com>
2016-01-21 16:50:54 +0200

    Create testi.cpp

---
+++ Core/testi.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+void failstestfunction()
+{
+    for (int i = 0; i < 10; i++)
+    {
+      printf("Olen funktio\n");
+    }
+}
+
+int main()
+{
+    printf("Hei maailma!\n");
+    
+    return 0;
+}
