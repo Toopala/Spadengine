7ccdf3028e019f208e20cb611975c70fba6e270c
Toopala <miika.saastamoinen@gmail.com>
2016-01-22 10:19:31 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
 call "C:\Program Files\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
 
-msbuild /t:Rebuild Spadengine.sln /property:Platform=Win32
+msbuild /t:Rebuild Build/Spadengine.sln /property:Platform=Win32
