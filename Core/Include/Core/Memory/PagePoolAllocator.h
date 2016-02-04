4462c2871f173f5a203d753f8abd12ff638501cd
LateXD <lauri.hankilanoja@hotmail.com>
2016-02-04 12:25:01 +0200

    Jotain pientä

--- Core/Include/Core/Memory/PagePoolAllocator.h
+++ Core/Include/Core/Memory/PagePoolAllocator.h
@@ -1,13 +1,14 @@
 #pragma once
 
 #include "Allocator.h"
+#include <stdlib.h>
 
 struct Page // A page contains a constant amount of same sized memory slots
 {
 	size_t slotSize;
 	unsigned slotCount, slotsLeft;
 	void *nextSlot;
-	Page *nextPageHeader;
+	Page *nextPage;
 };
 
 class PagePoolAllocator : public sge::Allocator
@@ -25,7 +26,23 @@ public:
 
 	void* allocate(size_t size)
 	{
-		
+		Page *page = NULL;
+		if (page->slotsLeft == 0)
+		{
+			// No room in current page, check if there's an another page with same sized slots and free space
+			Page *anotherPage = findPageWithRoom(page);
+			if (anotherPage == NULL)
+			{
+				// If page was not found, create a new page
+				Page *newPage = createNewPage(size);
+				page->nextPage = newPage;
+				page = newPage;
+			}
+			else
+			{
+				page = anotherPage;
+			}
+		}
 	}
 
 	void deallocate(void* data) 
@@ -33,7 +50,23 @@ public:
 
 	}
 
+	Page *findPageWithRoom(Page *page)
+	{
+
+	}
+
+	Page *createNewPage(size_t size)
+	{
+		// Creates a new page and reserves just enough space for all the blocks to fit
+		Page *page = (Page*) malloc(sizeof(Page) + size * poolSize);
+		page->slotSize = size;
+		page->slotCount = poolSize;
+		page->slotsLeft = poolSize;
+		page->nextSlot = page + 1;
+		page->nextPage = NULL;
+	}
+
 private:
 	size_t objectSize;
-	static const unsigned int poolSize = 100; // max items on one page
+	static const unsigned int poolSize = 100; // Max. number of items in one page
 };
