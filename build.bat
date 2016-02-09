788460653f6b7a2b18eb1db509d625fd0469db32
Toopala <miika.saastamoinen@gmail.com>
2016-02-09 15:20:09 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,3 +2,7 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
 call "C:\Program Files\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
 
 msbuild /t:Rebuild Build/Spadengine.sln /property:Platform=Win32
+
+if %ERRORLEVEL% NEQ 0 (
+	EXIT 1
+)
