9c61ea6ea113478e7fd41bb9322dec186c1b46c2
sathorael <sarielirc@gmail.com>
2016-02-11 14:49:56 +0200

    added test related classes, planning continues

--- Game/Source/Component.cpp
+++ Game/Source/Component.cpp
@@ -1,12 +1,13 @@
 #include "Game/Component.h"
 #include <assert.h>
-
+#include "Core\Assert.h"
 
 namespace sge
 {
 	Component::Component(Entity* ent) : parent(ent)
 	{
 		assert(ent != nullptr);
+		SGE_ASSERT(ent != nullptr);
 	}
 
 
---
+++ Game/Source/TestSystem.cpp
@@ -0,0 +1,11 @@
+#include "Game/TestSystem.h"
+
+
+TestSystem::TestSystem()
+{
+}
+
+
+TestSystem::~TestSystem()
+{
+}
