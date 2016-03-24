14edee0cf5ccc0c281991c42bf80dd7c25607d7a
LateXD <lauri.hankilanoja@hotmail.com>
2016-03-24 10:55:42 +0200

    Some quick fixes

--- Core/Include/Core/Random.h
+++ Core/Include/Core/Random.h
@@ -3,29 +3,10 @@
 
 namespace sge
 {
-	void randomInitialize() // Initializes with a "random" seed
-	{
-		srand(time(NULL));
-	}
-
-	void randomInitialize(const unsigned int &seed) // Initializes with the given seed
-	{
-		srand(seed);
-	}
-
-	int random() // Generates a random number
-	{
-		return rand();
-	}
-
-	int random(const int &min, const int &max) // Generates a random integer from range min-max
-	{
-		if (min < max)
-		{
-			return min + rand() % (max - min + 1);
-		}
-		return max + rand() % (min - max + 1);
-	}
+	void randomInitialize();
+	void randomInitialize(const unsigned int &seed);
+	int random();
+	int random(const int &min, const int &max);
 
 	template <typename T>
 	T random(const T &min, const T &max) // Generates a random float/double from range min-max
