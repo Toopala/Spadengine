5c63a7cdb969df74b386b3dcfc39fa8ce192b447
PetriTolonen <TTV14SPetriT@kamk.fi>
2016-04-08 10:24:00 +0300

    Camera Component update. BulletTestSene has now camera.

--- Samples/SceneSample/Source/BulletTestScene.cpp
+++ Samples/SceneSample/Source/BulletTestScene.cpp
@@ -205,6 +205,20 @@ BulletTestScene::BulletTestScene(sge::Spade* engine) : engine(engine)
 	fallRigidBody2 = new btRigidBody(fallRigidBodyCI2);
 	fallRigidBody2->setActivationState(DISABLE_DEACTIVATION);
 	dynamicsWorld->addRigidBody(fallRigidBody2);
+
+	EManager = new sge::EntityManager();
+	sysManager = new sge::SystemManager();
+	sysManager->init();
+	EManager->setSysManager(sysManager);
+	camentity = EManager->createEntity();
+
+	camcomponent = new sge::CameraComponent(camentity);
+	camentity->setComponent(camcomponent);
+
+	camtransform = new sge::TransformComponent(camentity);
+	camentity->setComponent(camtransform);
+
+	camentity->getComponent<sge::CameraComponent>()->setupCamera(45.0f, 16.0f / 9.0f, 0.1f, 1000.0f, false, cameraPos, cameraFront, cameraUp);
 }
 
 BulletTestScene::~BulletTestScene()
@@ -333,6 +347,18 @@ void BulletTestScene::update(float step)
 	{
 		engine->stop();
 	}
+
+	if (engine->keyboardInput->keyIsPressed(sge::KEYBOARD_F1))
+	{
+		camentity->getComponent<sge::CameraComponent>()->enableMouse();
+	}
+	if (engine->keyboardInput->keyIsPressed(sge::KEYBOARD_F2))
+	{
+		camentity->getComponent<sge::CameraComponent>()->disableMouse();
+	}
+
+	camentity->getComponent<sge::CameraComponent>()->update();
+	uniformData2.PV = camentity->getComponent<sge::CameraComponent>()->getVp();
 }
 void BulletTestScene::draw()
 {
