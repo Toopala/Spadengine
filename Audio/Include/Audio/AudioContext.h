e146e9e36c7aabda0a533acc311655dc15f830c3
Niko Kinnunen <nikokinnunen92@gmail.com>
2016-04-22 09:12:41 +0300

    AudioContext, some cleaning

--- Audio/Include/Audio/AudioContext.h
+++ Audio/Include/Audio/AudioContext.h
@@ -41,26 +41,24 @@ namespace sge
 
 		// Set position of the listener in the scene
 		// Default position is the (0, 0, 0).
-		static void setPosition(const sge::math::vec3 &position);
+		static void setPosition(const math::vec3 &position);
 
 		// Get the current position of the listener in the scene
-		static sge::math::vec3 getPosition();
+		static math::vec3 getPosition();
 
 		// Set the forward vector of the listener in the scene
-		static void setDirection(const sge::math::vec3 &direction);
+		static void setDirection(const math::vec3 &direction);
 
 		// Get the current forward vector of the listener in the scene
-		static sge::math::vec3 getDirection();
+		static math::vec3 getDirection();
 
 		// Set the upward vector of the listener in the scene
-		static void setUpVector(const sge::math::vec3 &upVector);
+		static void setUpVector(const math::vec3 &upVector);
 
 		// Get the current upward vector of the listener in the scene
-		static sge::math::vec3 getUpVector();
+		static math::vec3 getUpVector();
 
 		// Checking for OpenAL errors
 		void alCheckError(const char* file, unsigned int line, const char* expression);
-
-	private:
 	};
 }
