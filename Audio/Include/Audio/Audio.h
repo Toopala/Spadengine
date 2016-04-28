31798f52f2a08f23abd9577ac21a49696c3358ba
Niko Kinnunen <nikokinnunen92@gmail.com>
2016-04-28 15:06:53 +0300

    some little updates, learning PortAudio

--- Audio/Include/Audio/Audio.h
+++ Audio/Include/Audio/Audio.h
@@ -4,14 +4,21 @@
 
 namespace sge
 {	
+	typedef struct
+	{
+		float left_phase;
+		float right_phase;
+	}paTestData;
+
 	class Audio
 	{
 	public:
 		Audio();
-		~Audio()
-		{
+		~Audio();
+		typedef int paStreamCallBack(const void *input, void *output, unsigned long frameCount, const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags, void *userData);
+		static int paTestCallback(const void *inputBuffer, void *outputBuffer, unsigned long framesPerBuffer, const PaStreamCallbackTimeInfo *timeInfo, PaStreamCallbackFlags statusFlags, void *userData);
 
-		}
+		void initAudio();
 
 	private:
 	};
