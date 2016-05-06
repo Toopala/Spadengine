274a712889ddeb7eee20fa36efdb50a5e44b9ae0
Niko Kinnunen <nikokinnunen92@gmail.com>
2016-05-06 10:42:40 +0300

    PortAudio tested and it works.

--- Audio/Include/Audio/Audio.h
+++ Audio/Include/Audio/Audio.h
@@ -1,13 +1,16 @@
 #include "portaudio.h"
 #include "pa_asio.h"
 #include <stdio.h>
+#define TABLE_SIZE 200
 
 namespace sge
 {	
 	typedef struct
 	{
-		float left_phase;
-		float right_phase;
+		float sine[TABLE_SIZE];
+		int left_phase;
+		int right_phase;
+		char message[20];
 	}paTestData;
 
 	class Audio
@@ -17,7 +20,7 @@ namespace sge
 		~Audio();
 		typedef int paStreamCallBack(const void *input, void *output, unsigned long frameCount, const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags, void *userData);
 		static int paTestCallback(const void *inputBuffer, void *outputBuffer, unsigned long framesPerBuffer, const PaStreamCallbackTimeInfo *timeInfo, PaStreamCallbackFlags statusFlags, void *userData);
-
+		static void streamFinished(void* userData);
 		void initAudio();
 
 	private:
--- Audio/Source/Audio.cpp
+++ Audio/Source/Audio.cpp
@@ -1,6 +1,10 @@
 #include "Audio/Audio.h"
 #include <iostream>
+#include <math.h>
 #define SAMPLE_RATE 44100
+#define FRAMES_PER_BUFFER 64
+#define PI 3.1415
+#define NUM_SECONDS 1
 
 namespace sge
 {
@@ -16,33 +20,88 @@ namespace sge
 
 	void Audio::initAudio()
 	{
-		PaError err = Pa_Initialize();
+		PaStreamParameters outputParameters;
+		PaStream *stream;
+		PaError err;
+		paTestData data;
+
+		std::cout << "PortAudio Test: output sine wave. SR = " << SAMPLE_RATE << "BufSize = " << FRAMES_PER_BUFFER << std::endl;
+
+		// Initialise sinusoidal wavetable
+
+		for (int i = 0; i < TABLE_SIZE; i++)
+		{
+			data.sine[i] = (float)sin(((double)i / (double)TABLE_SIZE)*PI * 2);
+		}
+		data.left_phase = data.right_phase = 0;
+
+		err = Pa_Initialize();
 		if (err != paNoError)
 		{
 			fprintf(stderr, "Pa_Initialize error: %s\n", Pa_GetErrorText(err));
 		}
 
+		// Default output device
+		outputParameters.device = Pa_GetDefaultOutputDevice();
+		if (outputParameters.device == paNoDevice)
+		{
+			std::cout << "Error: No default output device." << std::endl;
+		}
+
+		outputParameters.channelCount = 2;	// Stereo output
+		outputParameters.sampleFormat = paFloat32;	// 32 bit floating point output
+		outputParameters.suggestedLatency = Pa_GetDeviceInfo(outputParameters.device)->defaultLowInputLatency;
+		outputParameters.hostApiSpecificStreamInfo = NULL;
+
+		err = Pa_OpenStream(
+			&stream,
+			NULL, // No input
+			&outputParameters,
+			SAMPLE_RATE,
+			FRAMES_PER_BUFFER,
+			paClipOff,      //We won't output out of range samples so don't bother clipping them
+			paTestCallback,
+			&data);
+		if (err != paNoError)
+		{
+			std::cout << "Error in opening stream" << Pa_GetErrorText(err) << std::endl;
+		}
+
 		// Get portaudio version
 		std::cout << Pa_GetVersionText() << std::endl;
 
-		PaStream *stream;
-
-		err = Pa_OpenDefaultStream(&stream, 0, 2, paFloat32, SAMPLE_RATE, 256, paTestCallback, &data);
+		/*err = Pa_OpenDefaultStream(&stream, 0, 2, paFloat32, SAMPLE_RATE, 256, paTestCallback, &data);
 		if (err != paNoError)
 		{
 			std::cout << "Error opening default stream: \n" << Pa_GetErrorText(err);
+		}*/
+
+		std::cout << data.message << " No message" << std::endl;
+		
+		err = Pa_SetStreamFinishedCallback(stream, &streamFinished);
+		if (err != paNoError)
+		{
+			std::cout << "Error setting stream finished callback: " << Pa_GetErrorText(err) << std::endl;
 		}
 
-		err = Pa_StartStream(&stream);
+		err = Pa_StartStream(stream);
 		if (err != paNoError)
 		{
 			std::cout << "Error starting stream: \n" << Pa_GetErrorText(err);
 		}
 
-		err = Pa_StopStream(&stream);
+		std::cout << "Paying for " << NUM_SECONDS << " seconds" << std::endl;
+		Pa_Sleep(NUM_SECONDS * 1000);
+
+		err = Pa_StopStream(stream);
 		if (err != paNoError)
 		{
 			std::cout << Pa_GetErrorText(err);
+		}
+
+		err = Pa_CloseStream(stream);
+		{
+			std::cout << "Error closing stream: " << Pa_GetErrorText(err) << std::endl;
 		}
 
 		err = Pa_Terminate();
@@ -50,6 +109,12 @@ namespace sge
 		{
 			std::cout << "PortAudio error: \n" << Pa_GetErrorText(err);
 		}
+	}
+
+	void Audio::streamFinished(void* userData)
+	{
+		paTestData *data = (paTestData*)userData;
+		std::cout << "Stream completed: " << data->message << std::endl;
 	}
 
 	int Audio::paTestCallback(const void *inputBuffer, void *outputBuffer,
@@ -61,20 +126,28 @@ namespace sge
 		paTestData *data = (paTestData*)userData;
 		float *out = (float*)outputBuffer;
 		unsigned int i;
+		(void)timeInfo;
+		(void)statusFlags;
 		(void)inputBuffer;
 
 		for (size_t i = 0; i < framesPerBuffer; i++)
 		{
-			*out++ = data->left_phase;  /* left */
-			*out++ = data->right_phase;  /* right */
+			*out++ = data->sine[data->left_phase];  /* left */
+			*out++ = data->sine[data->right_phase];  /* right */
 			/* Generate simple sawtooth phaser that ranges between -1.0 and 1.0. */
-			data->left_phase += 0.01f;
+			data->left_phase += 1;
 			/* When signal reaches top, drop back down. */
-			if (data->left_phase >= 1.0f) data->left_phase -= 2.0f;
+			if (data->left_phase >= TABLE_SIZE)
+			{
+				data->left_phase -= TABLE_SIZE;
+			}
 			/* higher pitch so we can distinguish left and right. */
-			data->right_phase += 0.03f;
-			if (data->right_phase >= 1.0f) data->right_phase -= 2.0f;
+			data->right_phase += 3;
+			if (data->right_phase >= TABLE_SIZE)
+			{
+				data->right_phase -= TABLE_SIZE;
+			}
 		}
-		return 0;
+		return paContinue;
 	}
 }
