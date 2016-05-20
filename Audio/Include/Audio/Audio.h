e0eecd1e0b33c1a40106cf35088d2a04324471f7
Niko Kinnunen <nikokinnunen92@gmail.com>
2016-05-20 19:47:07 +0300

    Audio: added doxy docymentation, 2 new audio files, and updated visualscene

--- Audio/Include/Audio/Audio.h
+++ Audio/Include/Audio/Audio.h
@@ -8,21 +8,53 @@
 
 namespace sge
 {	
+	/** \brief Audio class, that handles audio play, loop and stop.
+	*
+	*	Engine uses PortAudio and libsndfile for audi I/O. Audio is only for stereo..
+	*	Audio can play .wav/flac files.
+	*/
 	class Audio
 	{
 	public:
+		/** \brief The default constructor. */
 		Audio();
+		/** \brief The destructor. */
 		~Audio();
 
+		/** \brief Plays single audio file for 1 time
+		*
+		*	Plays .wav/.flac file.
+		*	\param string soundFile : Audio file name.
+		*/
 		void play(std::string soundFile);
+
+		/** \brief Loops single audio file.
+		*
+		*	Loops .wav/flac file.
+		*	\param string soundFile : Audio file name.
+		*/
 		void loop(std::string soundFile);
+
+		/** \brief Stops audio
+		*
+		*	Stops all the audio files from playing or looping.
+		*/
 		void stop();
 
+		/** \brief Contains audio
+		*
+		*	Checks, if the map contains any audio.
+		*/
 		bool containsAudio(std::string fileName);
+
+		/** \brief Method, that loads audiofile
+		*
+		*	Loads audio file and gives error if it's unable to open audio file.
+		*/
 		AudioFile &getAudio(std::string fileName);
 
 	private:
-		std::map<std::string, AudioFile> sounds;
-		AudioStreamHandler audioStreamHandler;
+		std::map<std::string, AudioFile> sounds;	/**< Map that contains all the audio */
+		AudioStreamHandler audioStreamHandler;		/**< Handler for all audio data. */
 	};
 }
--- Audio/Include/Audio/AudioStreamHandler.h
+++ Audio/Include/Audio/AudioStreamHandler.h
@@ -9,40 +9,68 @@
 
 namespace sge
 {
+	/** \brief Hold information about audio data. */
 	struct AudioFile
 	{
-		SNDFILE *data;
-		SF_INFO info;
+		SNDFILE *data;	/**< Pointer to audio data. */
+		SF_INFO info;	/**< Passing data between the calling function and the library when opening a file for reading or writing. */
 	};
 
+	/** \brief Playback for audio. */
 	struct Playback
 	{
-		AudioFile *audiofile;
-		int position;
-		bool loop;
+		AudioFile *audiofile; /**< Pointer to audio file. */
+		int position;		  /**< Position of the sound. */
+		bool loop;			  /**< Loop for the audio. */
 	};
 
+	/** \brief Audio event types. */
 	enum AudioEventType
 	{
 		start,
 		stop
 	};
 
+	/** \brief Class for handling all the audio data.
+	*
+	*	Class handles all the audiodata, initializes PortAudio and uses libsndfile for writing audio data.
+	*	
+	*/
 	class AudioStreamHandler
 	{
 	public:
+		/** \brief The default constructor.
+		*
+		*	Initializes the PortAudio and gives parameters to the PortAudio device.
+		*/
 		AudioStreamHandler();
+
+		/** \brief The destructor.
+		*
+		*	Closes PortAudio stream, deletion of all the audio data and terminates the device.
+		*/
 		~AudioStreamHandler();
 
+		/** \brief Process the audio events.
+		*
+		*	Handles all the audio events: start and stop.
+		*	Use in Audio.cpp to play, loop or stop audio.
+		*/
 		void processEvent(AudioEventType audioEventType, AudioFile *audiofile = nullptr, bool loop = false);
+
+		/** \brief PortAudio stream callback.
+		*
+		*	Consumes, process and generates audio.
+		*
+		*/
 		static int paStreamCallBack(const void *input, void *output, unsigned long frameCount, const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags, void *userData);
 
 	private:
-		const int CHANNEL_COUNT = 2;
-		const int SAMPLE_RATE = 44100;
-		const PaStreamParameters *NO_INPUT = nullptr;
+		const int CHANNEL_COUNT = 2;						/**< Channel count. */
+		const int SAMPLE_RATE = 44100;						/**< Sample rate (44100). */
+		const PaStreamParameters *NO_INPUT = nullptr;		/**< Pointer to PortAudio stream parameters. */
 
-		PaStream *stream;
-		std::vector<Playback*> data;
+		PaStream *stream;									/**< Pointer to stream. */
+		std::vector<Playback*> data;						/**< Pointer to Playback vector. */
 	};
 }
